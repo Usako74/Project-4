4faa46be103b29d149512b9a66b1c70ed60202fc
Usako <ophelie.bourdon@gmail.com>
2019-04-22 10:02:30 +0200

    CSS and Template
    Create style.css and Change template.php

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -5,5 +5,17 @@
  * Date: 22/04/2019
  * Time: 09:19
  */
+?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8" />
+    <title><?= $title ?></title>
+    <link href="/public/css/style.css" rel="stylesheet" />
+</head>
 
+<body>
+<?= $content ?>
+</body>
+</html>
