c8278bc6a3594257970c7742b0274e71faea4fe6
Usako <ophelie.bourdon@gmail.com>
2019-04-23 10:39:58 +0200

    controller & view
    Add article system  and function on controller

--- controller/frontend.php
+++ controller/frontend.php
@@ -5,8 +5,12 @@
  * Date: 22/04/2019
  * Time: 10:26
  */
+// Loading Class
+require_once('model/PostManager.php');
 
 function listPosts()
 {
+    $postManager = new PostManager(); // Create Object
+    $posts = $postManager->getPost(); // Call function of this Object
     require('view/frontend/listPostsView.php');
 }
--- view/frontend/listPostsView.php
+++ view/frontend/listPostsView.php
@@ -13,6 +13,26 @@
 <h1>Titre de la liste des articles</h1>
 <p>Liste des articles:</p>
 
+<?php
+while ($data = $posts->fetch())
+{
+?>
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($data['title']) ?>
+            <b>le <?= $data['create_date_fr'] ?></b>
+        </h3>
+
+        <p>
+            <?= nl2br(htmlspecialchars($data['content'])) ?>
+            <br>
+            <b><a href="index.php?action=post&amp;id=<?= $data['id'] ?>">Afficher les commentaires</a></b>
+        </p>
+    </div>
+<?php
+}
+$posts->closeCursor();
+?>
 
 
 <?php $content = ob_get_clean(); ?>
