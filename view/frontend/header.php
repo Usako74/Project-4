2ded08bb9181798e01b75d324de954ff3689544b
Usako <ophelie.bourdon@gmail.com>
2019-04-25 16:07:14 +0200

    CSS header and template
    add CSS, header.php with slider and update template.php

---
+++ view/frontend/header.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Usako
+ * Date: 25/04/2019
+ * Time: 12:23
+ */
+?>
+<header class="z-center-block">
+    <h1 class="text-center">Billet simple pour l'Alaska</h1>
+    <div class="slider">
+        <div class="display-flex">
+            <figure class="showslide position-absolute full-size">
+                <img src="public/img/pinguin.jpg" alt="Illustration de pinguins sur la neige" class="full-size">
+            </figure>
+            <figure  class="showslider position-absolute full-size transparency">
+                <img src="public/img/igloo.jpg" alt="Illustration d'un igloo au milieu de la neige" class="full-size">
+            </figure>
+            <figure  class="showslider position-absolute full-size transparency">
+                <img src="public/img/cerf&biche.jpg" alt="Illustration d'un cerf et d'une biche sur la neige" class="full-size">
+            </figure>
+        </div>
+    </div>
+</header>
--- view/frontend/template.php
+++ view/frontend/template.php
@@ -10,12 +10,20 @@
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-    <meta charset="utf-8" />
+    <meta charset="utf-8">
     <title><?= $title ?></title>
-    <link href="public/css/style.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Jura" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+    <link href="public/css/style.css" rel="stylesheet">
 </head>
 
 <body>
+<?php include('header.php'); ?>
+
 <?= $content ?>
+
+<?php include('footer.php'); ?>
 </body>
 </html>
