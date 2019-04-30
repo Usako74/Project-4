ed87ad1f5c3e296e926a422c015ebb4998d202cc
Usako <ophelie.bourdon@gmail.com>
2019-04-30 11:33:22 +0200

    footer
    Add footer with scripts

---
+++ view/frontend/footer.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Usako
+ * Date: 25/04/2019
+ * Time: 14:01
+ */
+?>
+<footer>
+
+</footer>
+
+<script src="public/js/Slider.js"></script>
+<script src="public/js/script.js"></script>
