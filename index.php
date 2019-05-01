96de6107560be5af2d52c89a8dc285c83490f612
Usako <ophelie.bourdon@gmail.com>
2019-05-01 12:57:01 +0200

    index.php
    update index.php, add page&error system

--- index.php
+++ index.php
@@ -7,8 +7,36 @@
  */
 require('controller/frontend.php');
 try {
-    listPosts();
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    throw new Exception('Tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+    }
+    else {
+        listPosts();
+    }
 }
 catch (Exception $e) {
-    echo 'Une erreur c\'est produite : <br>' . $e->getMessage();
+    echo 'Une erreur s\'est produite : <br>' . $e->getMessage();
 }
