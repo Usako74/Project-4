4e38d5af92e758e8b01c40d76a8ba984a28ec703
Usako <ophelie.bourdon@gmail.com>
2019-05-01 12:55:08 +0200

    frontend.php
    update file, add functions

--- controller/frontend.php
+++ controller/frontend.php
@@ -7,10 +7,36 @@
  */
 // Loading Class
 require_once('model/PostManager.php');
+require_once('model/CommentManager.php');
 
 function listPosts()
 {
     $postManager = new PostManager(); // Create Object
-    $posts = $postManager->getPost(); // Call function of this Object
+    $posts = $postManager->getPosts(); // Call function of this Object
     require('view/frontend/listPostsView.php');
+}
+
+function post()
+{
+    $postManager = new PostManager();
+    $commentManager = new CommentManager();
+
+    $post = $postManager->getPost($_GET['id']);
+    $comments = $commentManager->getComments($_GET['id']);
+
+    require('view/frontend/postView.php');
+}
+
+function addComment($postId, $author, $comment)
+{
+    $commentManager = new CommentManager();
+
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
+
+    if ($affectedLines === false) {
+        throw new Exception('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $postId);
+    }
 }
